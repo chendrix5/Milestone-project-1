30a09b1b6c4b6d6d807fd4776c293cd937df5e6e
clay hendrix <clayhendrix5@gmail.com>
2022-09-26 15:28:16 -0500

    java func

--- java.js
+++ java.js
@@ -2,14 +2,28 @@ let userName = document.querySelector("#userName")
 let headerText = document.querySelector("#headerText")
 let subBox = document.querySelector("#subBox")
 let Quiz = document.querySelector("#Quiz")
+let Q1Button = document.querySelector("#Q1Button")
+let Q2Button = document.querySelector("#Q2Button")
+let Q3Button = document.querySelector("#Q3Button")
+let Question = document.querySelector("#Question")
 let A1 = document.querySelector("#A1")
 let A2 = document.querySelector("#A2")
 let A3 = document.querySelector("#A3")
 let A4 = document.querySelector("#A4")
+let iterationNumber = 0;
 let qnChoice = "";
 let score = 0;
 
+let questionList = [
+    "Qn1: What was Frank's nickname at his old company?", 
+    "Qn2: What is Dee's nickname given to her, by the gang?",
+    "Qn3: In the episode where charlie goes on a date and sweats because he ate too much cheese, what does he try to say his job is?"
+]
+
 Quiz.style.display = "none";
+Q1Button.style.display = "none";
+Q2Button.style.display = "none";
+Q3Button.style.display = "none";
 
 
 function greeting(){
@@ -17,6 +31,8 @@ function greeting(){
     headerText.innerHTML = "Hello " + userName.value + ", Welcome to Kellys Pub!"
     subBox.style.display = "none"
     Quiz.style.display = "block";
+    Q1Button.style.display = "block";
+    Question.innerHTML = questionList[iterationNumber];
 }
 
 function aFunction(){
@@ -50,4 +66,44 @@ function dFunction(){
     A4.style.color = "Red"; 
     qnChoice = "d"
 }
-function submitQn1()
+function submitQn1(){
+    if (qnChoice == "d"){
+        score = score + 1;
+    }
+    iterationNumber += 1;
+    Question.innerHTML = questionList[iterationNumber];
+    Q1Button.style.display = "none";
+    Q2Button.style.display = "block";
+    A1.style.color = "White";
+    A2.style.color = "white";
+    A3.style.color = "White";
+    A4.style.color = "White";
+}
+
+function submitQn2(){
+    if (qnChoice == "a"){
+        score = score + 1;
+    }
+    iterationNumber += 1;
+    Question.innerHTML = questionList[iterationNumber];
+    Q2Button.style.display = "none";
+    Q3Button.style.display = "block";
+    A1.style.color = "White";
+    A2.style.color = "white";
+    A3.style.color = "White";
+    A4.style.color = "White";
+}
+
+function submitQn3(){
+    if (qnChoice == "c"){
+        score = score + 1;
+    }
+    iterationNumber += 1;
+    headerText.innerHTML = "Your score is " + score;
+    Quiz.style.display = "none";
+    A1.style.color = "White";
+    A2.style.color = "white";
+    A3.style.color = "White";
+    A4.style.color = "White";
+    
+}
